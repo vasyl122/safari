4dbab3ea166eced99af8c710c3ef7797385aee16
vasyl122 <79007397+vasyl122@users.noreply.github.com>
2021-12-16 11:22:25 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(function () {
+   $('.popap__link').magnificPopup({
+      disableOn: 700,
+      type: 'iframe',
+      mainClass: 'mfp-fade',
+      removalDelay: 160,
+      preloader: false,
+
+      fixedContentPos: false
+   });
+   $('.gellery__slider').slick({
+      prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/errov-left.svg" alt="#"></button>',
+      nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/errov-riht.svg" alt="#"></button>',
+   });
+   $('.gellery__item-inner').magnificPopup({
+      delegate: 'a',
+      type: 'image',
+      tLoading: 'Loading image #%curr%...',
+      mainClass: 'mfp-img-mobile',
+      gallery: {
+         enabled: true,
+         navigateByImgClick: true,
+         preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+      },
+
+   });
+});
